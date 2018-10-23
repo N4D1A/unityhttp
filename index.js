b17fa124499c4e48c5e41e04bc85402bc1608285
N4D1A <35233687+N4D1A@users.noreply.github.com>
2018-10-24 03:23:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 5000, function () {
 app.use(bodyParser.json()); //
 app.use(bodyParser.urlencoded({ extended: true })); //
 
-var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
+// var obj = JSON.parse(fs.readFileSync('info.json', 'utf8'));
 
 app.get('/', function(req, res){ // 사용자가 홈페이지 홈(/)으로 접속하면, 실행될 콜백함수 지정. 요청에 대한 객체, 응답에 대한 객체 전달
   res.send('Hello, This is AR Docent Webserver'); // send()의 인자로 전달된 값을 응답하게 됨
